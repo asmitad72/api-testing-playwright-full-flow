768ad536c45c9dea96a367ae359ba25ca4434ae8
Asmita <asmitad72@gmail.com>
2026-01-20 13:36:45 +0530

    order creation with helpers

--- tests/specs/api/get-orders.spec.ts
+++ tests/specs/api/get-orders.spec.ts
@@ -1,5 +1,4 @@
 import { expect, test } from '@playwright/test'
-//import { fetchJwt, getAllOrders } from '../../helpers/api-helper'
 import {
   createOrder,
   deleteAnOrder,
@@ -9,27 +8,25 @@ import {
 } from '../../helpers/api-helper'
 
 test('Get all orders', async ({ request }) => {
-  //Get JWT token
   const jwt = await fetchJwt(request)
   const orders = await getAllOrders(request, jwt)
-  expect(Array.isArray(orders))
+
+  expect(Array.isArray(orders)).toBe(true)
   expect(orders.length).toBeGreaterThanOrEqual(0)
 })
 
-test ( "Delete an order", async ({request} ) => {
-  // get jwt token
-  const jwt = await fetchJwt(request);
-  const orderId = await createOrder(request, jwt);
-  const response = await deleteAnOrder(request, jwt, orderId);
-  console.log(response);
-  expect(response).toBe(true);
-});
+test('Delete an order', async ({ request }) => {
+  const jwt = await fetchJwt(request)
+  const orderId = await createOrder(request, jwt)
 
-test.only ( "Student cannot update order", async ({request} ) => {
-  // get jwt token
-  const jwt = await fetchJwt(request);
-  const orderId = await createOrder(request, jwt);
-  const response = await updateOrder(request, jwt, orderId, "INPROGRESS");
-  console.log(response);
-  expect(response.status()).toBe(403);
-});
+  const response = await deleteAnOrder(request, jwt, orderId)
+  expect(response).toBe(true)
+})
+
+test('Student cannot update order', async ({ request }) => {
+  const jwt = await fetchJwt(request)
+  const orderId = await createOrder(request, jwt)
+
+  const response = await updateOrder(request, jwt, orderId, 'INPROGRESS')
+  expect(response.status()).toBe(403)
+})
